e051444a29333b8767b3a3183c86e83bbb278cb2
Precious Agbagharauka <clickpresh1@gmail.com>
2025-09-18 19:08:24 +0100

    Update FirstBadVersion.java

--- FirstBadVersion.java
+++ FirstBadVersion.java
@@ -1,34 +1,19 @@
-// DRAFT
-// DRAFT
-// DRAFT
-
-
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int val = 0;
+        int val = 1;
         int high = n;
         int low = 1;
-        while (true) {
+        while (low <= high) {
             int mid = low + ((high - low) / 2);
-            if (!(isBadVersion(mid - 1)) && (isBadVersion(mid))) {
+            if (isBadVersion(mid)) {
                 val = mid;
-                break;
-            } else if ((isBadVersion(mid - 1)) && (isBadVersion(mid))) {
                 high = mid - 1;
-            } else if ((!(isBadVersion(mid - 1))) && (!(isBadVersion(mid)))) {
+            } else if (!(isBadVersion(mid))) {
                 low = mid + 1;
-            } 
-
-            // if (isBadVersion(mid)) {
-            //     high = mid - 1;
-            // } else if (!(isBadVersion(mid))) {
-            //     low = mid + 1;
-            // }
-
-
+            }
         } 
 
         return val;
