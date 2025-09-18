65f065b6c110577c3f32466e7a61dabe717760eb
Precious Agbagharauka <clickpresh1@gmail.com>
2025-09-18 11:33:50 +0100

    Update UglyNumber.java

--- UglyNumber.java
+++ UglyNumber.java
@@ -1,29 +1,35 @@
 class Solution {
     public boolean isUgly(int n) {
         boolean isUgly = false;
+
         if (n <= 0) {
             isUgly = false;
-        }
-        if ((n >= 1) && (n <= 5)) {
+        } else if (n == 1) {
             isUgly = true;
-        }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                if ((!((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0)))) {
+        } else {
+            while (true) {
+                if ((!(n % 2 == 0)) && (!(n % 3 == 0)) && (!(n % 5 == 0))) {
                     isUgly = false;
                     break;
-                } else if ((((((n / i) % 2) != 0) && (((n / i) % 3) != 0) && (((n / i) % 5) != 0)))  && ((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0))) {
-                    isUgly = false;
-                    break;
-                } else if ((!((((n / i) % 2) != 0) && (((n / i) % 3) != 0) && (((n / i) % 5) != 0)))  && ((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0))) {
-                    System.out.println(i);
-                    isUgly = true;
                 } 
+                if (n % 2 == 0) {
+                    n = (n / 2);
+                } else if (n % 3 == 0) {
+                    n = (n / 3);
+                } else if (n % 5 == 0) {
+                    n = (n / 5);
+                } 
+
+                if (n == 1) {
+                    isUgly = true;
+                    break;
+                }
             }
         }
-
+        
         return isUgly;
     } 
+
 
     
 }
