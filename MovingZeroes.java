219460ba10ccb33fff4b56dea0dea1e57a6f1e2a
Precious Agbagharauka <clickpresh1@gmail.com>
2025-09-19 14:14:12 +0100

    Create MovingZeroes.java

---
+++ MovingZeroes.java
@@ -0,0 +1,23 @@
+
+import java.util.Arrays;
+
+class Solution {
+    public static void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if ((nums[i] == 0) && (i + 1 < nums.length)) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] != 0) {
+                        int helper = nums[j];
+                        nums[j] = nums[i];
+                        nums[i] = helper;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+        
+    } 
+
+}
