1f74c55bfca6a03931c2d1f15a9133a511d0d8d3
Precious Agbagharauka <clickpresh1@gmail.com>
2025-09-20 18:58:31 +0100

    Update LongestSubstringWithoutRepeatingCharacters.java

--- LongestSubstringWithoutRepeatingCharacters.java
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -1,82 +1,32 @@
-// DRAFT
-// DRAFT
-// DRAFT
-
-
-class Solution {
-    public static int lengthOfLongestSubstring(String s) {
-        char[] sArr = s.toCharArray();
-        int firstInd = 0;
-        int secondInd = 1;
-        int diff = 0;
-        int val = 1;
-        int output = 0;
-        for (int i = 0; i < sArr.length; i++) {
-            if ((sArr[i] == sArr[firstInd])) {
-                diff = secondInd - firstInd;
-                output = Math.max(val, diff);
-                firstInd = i;
-                secondInd = i + 1;
-                
-                // secondInd = i;
-            } else {
-                if (i + 1 < sArr.length) {
-                    if (sArr[i + 1] != sArr[i]) {
-                        secondInd = i + 1;
-                    } else {
-                        firstInd = i + 1;
-                        secondInd = (i + 1) + 1;
-                        continue;
-                    }
-                } else if (i + 1 == sArr.length) {
-                    secondInd = i + 1;
-                    diff = secondInd - firstInd;
-                    output = Math.max(val, diff);
-                }
-
-            }
-        
-
-            val = diff;
-    }
-
-        return output;
-    } 
-
-    public static void main(String[] args) {
-        // System.out.println(lengthOfLongestSubstring("pwwkew"));
-        System.out.println(lengthOfLongestSubstring("tmmzuxt"));
-    }
-} 
-
-
-// import java.util.ArrayList;
-// import java.util.Arrays;
-// import java.util.HashSet;
-// import java.util.List;
-// import java.util.Set;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int val = 0;
         int output = 0;
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j < s.length(); j++) {
-                String sub = s.substring(i, j + 1);
-                char[] subCharArr = sub.toCharArray();
-                Set<Character> subSet = new HashSet<>();
-                for (Character k : subCharArr) {
-                    subSet.add(k);
-                } 
-                if (subSet.size() == subCharArr.length) {
-                    val = subSet.size();
-                    output = Math.max(output, val);
-                }
+        char[] sCharArr = s.toCharArray();
+        if (s.length() < 2) {
+            output = s.length();
+        } 
 
-            }
-        }
+        int L = 0;
+        int R = 0;
+
+        HashMap<Character, Integer> hashmap = new HashMap<>();
+
+        while (R < s.length()) {
+            hashmap.put(sCharArr[R], hashmap.getOrDefault(sCharArr[R], 0) + 1);
+
+            while (hashmap.size() != R - L + 1) {
+                hashmap.put(sCharArr[L], hashmap.get(sCharArr[L]) - 1);
+                if (hashmap.get(sCharArr[L]) == 0) {
+                    hashmap.remove(sCharArr[L]);
+                } 
+                L++;
+            } 
+            output = Math.max(output, R - L + 1);
+
+            R++;
+        } 
 
         return output; 
     }
-
 }
