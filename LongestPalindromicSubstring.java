26b3041f2bcadaafe86e1b4c05aca6baf5a9a0d0
Precious Agbagharauka <clickpresh1@gmail.com>
2025-09-21 15:53:42 +0100

    Update LongestPalindromicSubstring.java

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -1,1 +1,24 @@
 
+class Solution {
+    public static String longestPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('@');
+        sb.append('#');
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+            sb.append('#');
+        }
+        sb.append('$');
+
+        String sSb = sb.toString(); 
+
+        int[] arr = new int[sSb.length()];
+        
+        return "";
+    } 
+
+    public static void main(String[] args) {
+        String s = "ab";
+        System.out.println(longestPalindrome(s));
+    }
+}
