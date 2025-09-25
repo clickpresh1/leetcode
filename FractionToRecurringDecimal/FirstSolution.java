9a110cdfb9529d6ebfb70a4f7559b202fdcfa378
Precious Agbagharauka <clickpresh1@gmail.com>
2025-09-25 07:32:49 +0100

    Update FirstSolution.java

--- FractionToRecurringDecimal/FirstSolution.java
+++ FractionToRecurringDecimal/FirstSolution.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 
 class Solution {
-    public static String fractionToDecimal(int numerator, int denominator) {
+    public String fractionToDecimal(int numerator, int denominator) {
         StringBuilder sb = new StringBuilder();
         String result = "";
         String sign = "";
@@ -20,41 +20,27 @@ class Solution {
         
         long wholeNumber = absNum / absDen;
         sb.append(wholeNumber);
-        result += wholeNumber;
         if (!(absNum % absDen == 0)) {
-            sb.append(longDivision(absNum, absDen));
+            sb.append(".");
+            HashMap<Long, Integer> hashMap = new HashMap<>();
+            long rem = (absNum % absDen);
+            while (rem != 0) {
+                if (hashMap.containsKey(rem)) {
+                    int ind = hashMap.get(rem);
+                    sb.insert(ind, "(");
+                    sb.append(")");
+                    break;
+                } 
+
+                hashMap.put(rem, sb.length());
+                rem = rem * 10;
+                sb.append(rem / absDen);
+                rem = rem % absDen;
+            } 
         } 
 
-        result = sb.toString();
-
-        result = sign + result.replaceAll("-", "");
+        result = sign + sb.toString().replaceAll("-", "");
 
         return result;
     } 
-
-    public static String longDivision(long a, long b) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(".");
-        HashMap<Long, Integer> hashMap = new HashMap<>();
-        long den = b;
-        long rem = (a % den);
-        while (rem != 0) {
-            if (hashMap.containsKey(rem)) {
-                int ind = hashMap.get(rem);
-                sb.insert(ind, "(");
-                sb.append(")");
-                break;
-            } 
-
-            hashMap.put(rem, sb.length());
-            rem = rem * 10;
-            sb.append(rem / den);
-            rem = rem % den;
-        } 
-    
-        String s = sb.toString();
-
-        return s;
-    }
-
 }
