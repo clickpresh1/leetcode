ff1873a6b8b24e77acb5bbf696ba7034948adb89
Precious Agbagharauka <clickpresh1@gmail.com>
2025-09-29 14:55:14 +0100

    Create FirstSolution.java

---
+++ RotateImage/FirstSolution.java
@@ -0,0 +1,40 @@
+// Swap rows and transpose matrix, and brute force
+// Swap rows and transpose matrix, and brute force
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Solution {
+    public int[][] rotate(int[][] nums){
+        HashSet<ArrayList<Integer>> hashSet = new HashSet<>();
+        for (int i = 0; i < nums.length / 2; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                int helper = nums[i][j];
+                nums[i][j] = nums[nums.length - 1 -i][j];
+                nums[nums.length - 1 - i][j] = helper;
+            }
+        } 
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(i);
+                list.add(j);
+                Collections.sort(list);
+                if (!(hashSet.contains(list))) {
+                    int helper = nums[i][j];
+                    nums[i][j] = nums[j][i];
+                    nums[j][i] = helper;
+
+                    hashSet.add(list);
+                }
+                
+            }
+            
+        }
+        
+        return nums;
+    }
+}
