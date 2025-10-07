fbba5d58e3dd01f7fc2e34967fe918d4bb26298f
Precious Agbagharauka <clickpresh1@gmail.com>
2025-10-07 17:10:23 +0100

    Update Memoization_Recursion.java

--- UniquePaths2/Memoization_Recursion.java
+++ UniquePaths2/Memoization_Recursion.java
@@ -3,20 +3,20 @@ class Solution {
         return uniquePathsWithObstacles(0, 0, new HashMap<>(), obstacleGrid);
     } 
 
-    public static int uniquePathsWithObstacles(int row, int col, HashMap<String, Integer> hashMap, int[][] obstacleGrid) { 
+    public static int uniquePathsWithObstacles(int row, int col, HashMap<List<Integer>, Integer> hashMap, int[][] obstacleGrid) { 
         if ((row == obstacleGrid.length) || (col == obstacleGrid[0].length)) return 0;
 
         if (obstacleGrid[row][col] == 1) return 0;
 
         if ((row == obstacleGrid.length - 1) && (col == obstacleGrid[0].length - 1)) return 1;
 
-        if (hashMap.containsKey(row + "_" + col)) {
-            return hashMap.get(row + "_" + col);
+        List<Integer> list = List.of(row, col);
+        if (hashMap.containsKey(list)) {
+            return hashMap.get(list);
         }
 
         int result = uniquePathsWithObstacles(row + 1, col, hashMap, obstacleGrid) + uniquePathsWithObstacles(row, col + 1, hashMap, obstacleGrid);
-        String keyVal = row + "_" + col;
-        hashMap.put(keyVal, result);
+        hashMap.put(list, result);
         
         return result;
     } 
