c75f3fe6b85994f2fc5fe3f81ab25edfbaa3109b
Precious Agbagharauka <clickpresh1@gmail.com>
2025-10-16 22:54:25 +0100

    Create FrequencyArray_BooleanArray.java

---
+++ SmallestMissingNonNegativeIntegerAfterOperations/FrequencyArray_BooleanArray.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int findSmallestInteger(int[] nums, int value) {
+        int n = nums.length;
+        int[] freq = new int[value];
+        for (int i = 0; i < n; i++) {
+            int a = Math.floorMod(nums[i], value);
+            nums[i] = a + (freq[a] * value);
+            freq[a] += 1;
+        }
+
+        return firstMissingPositive(nums);
+    } 
+
+    public static int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        boolean[] boos = new boolean[n + 1];
+        
+        int currBoolFalseIndex = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            if (x <= n) {
+                boos[x] = true;
+                if (x == currBoolFalseIndex) {
+                    currBoolFalseIndex += 1;
+                }
+            } 
+        }
+
+        while (currBoolFalseIndex < n) {
+            if (boos[currBoolFalseIndex] == true) {
+                currBoolFalseIndex += 1;
+            } else break;   
+        }
+
+        return currBoolFalseIndex;
+    } 
+} 
