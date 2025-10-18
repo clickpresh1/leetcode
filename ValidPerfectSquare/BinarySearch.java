b62911b489e3254c25e8228f7945f07ad6465d14
Precious Agbagharauka <clickpresh1@gmail.com>
2025-10-18 07:37:25 +0100

    Update BinarySearch.java

--- ValidPerfectSquare/BinarySearch.java
+++ ValidPerfectSquare/BinarySearch.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isPerfectSquare(int num) {
         if (num == 1) return true;
         int x = 2;
-        int y = num / 2; // would normally have y = num here (or y = num - 1), but since the square root cannot be greater than the half of the number, i decided to use y = num / 2
+        int y = (num / 2) + 1; // would normally have y = num here (or y = num - 1), but since the square root cannot be greater than the half of the number, + 1, i decided to use y = num / 2
 
         while (x <= y) {
             int mid = x + ((y - x) / 2);
