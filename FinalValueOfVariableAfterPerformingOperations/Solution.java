99fbfd4195ac34f14f6aec667547f006d69a3220
Precious Agbagharauka <clickpresh1@gmail.com>
2025-10-21 06:57:13 +0100

    Create Solution.java

---
+++ FinalValueOfVariableAfterPerformingOperations/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int m = operations.length;
+        int x = 0;
+        for (int i = 0; i < m; i++) {
+            if (operations[i].charAt(0) == '+' || operations[i].charAt(2) == '+') x += 1;
+            else x -= 1;
+        } 
+
+        return x;
+    }
+}
