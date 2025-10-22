4da678c529777306c45bb34659f6dc77dd9fd164
Precious Agbagharauka <clickpresh1@gmail.com>
2025-10-22 07:52:52 +0100

    Create DP_With_Array.java

---
+++ ClimbingStairs/DP_With_Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] arr = new int[n + 1];
+        if (n == 1 || n == 2) return n;
+        arr[1] = 1;
+        arr[2] = 2;
+        for (int i = 3; i < n + 1; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        return arr[n];
+    } 
+}
