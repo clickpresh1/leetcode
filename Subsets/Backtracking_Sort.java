aa2a2bfb245dea72f032155919fbedaad8f2dcad
Precious Agbagharauka <clickpresh1@gmail.com>
2025-10-26 09:44:35 +0100

    Create Backtracking_Sort.java

---
+++ Subsets/Backtracking_Sort.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        list.add(new ArrayList<>());
+        Arrays.sort(nums);
+
+        backtrack(nums, list, new ArrayList<>(), 0);
+
+        return list;
+    } 
+
+    public static void backtrack(int[] nums, List<List<Integer>> list, List<Integer> temp, int x) {
+        for (int i = x; i < nums.length; i++) {
+            temp.add(nums[i]);
+            list.add(new ArrayList<>(temp));
+            backtrack(nums, list, temp, i + 1);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
