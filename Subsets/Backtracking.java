d901d3e7f67dd2bca9eba8413ab9891edc7fad99
Precious Agbagharauka <clickpresh1@gmail.com>
2025-10-26 09:21:43 +0100

    Create Backtracking.java

---
+++ Subsets/Backtracking.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        list.add(new ArrayList<>());
+
+        backtrack(nums, list, new ArrayList<>(), 0);
+
+        return list;
+    } 
+
+    public static void backtrack(int[] nums, List<List<Integer>> list, List<Integer> temp, int x) {
+        if ((temp.size() == nums.length) && (!(list.contains(temp)))) {
+            list.add(new ArrayList<>(temp));
+            return;
+        }
+        else {
+            for (int i = x; i < nums.length; i++) {
+                temp.add(nums[i]);
+                if (!(list.contains(temp))) list.add(new ArrayList<>(temp));
+                backtrack(nums, list, temp, i + 1);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+}
