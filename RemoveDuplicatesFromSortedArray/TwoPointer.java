0cf8244c64dd26dcd193c5d46859a79cc3310ad1
Precious Agbagharauka <clickpresh1@gmail.com>
2025-10-28 08:00:35 +0100

    Create TwoPointer.java

---
+++ RemoveDuplicatesFromSortedArray/TwoPointer.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int m = nums.length;
+        int x = 0;
+        int i = 0;
+        int count = 0;
+        nums[x] = nums[0];
+        x += 1;
+        count += 1;
+        i += 1;
+
+        while (i < m) {
+            if (nums[i] == nums[i - 1]) {
+                if (count < 2) {
+                    nums[x] = nums[i];
+                    x += 1;
+                    count += 1;
+                    i += 1;
+                } else {
+                    i += 1;
+                } 
+            } else {
+                count = 0;
+                nums[x] = nums[i];
+                x += 1;
+                count += 1;
+                i += 1;
+            }
+        }
+        
+        return x;
+    }
+}
