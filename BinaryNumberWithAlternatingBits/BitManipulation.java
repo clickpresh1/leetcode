10182559f30e9af0f7fda6b8bb5530145f354968
Precious Agbagharauka <clickpresh1@gmail.com>
2025-11-02 16:23:44 +0100

    Update BitManipulation.java

--- BinaryNumberWithAlternatingBits/BitManipulation.java
+++ BinaryNumberWithAlternatingBits/BitManipulation.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean hasAlternatingBits(int n) {
-        int x = n;
-        int b = x & 1;
+        int x = n, b = x & 1;
 
-        int y = n >> 1;;
-        int c = y & 1;
+        int y = n >> 1, c = y & 1;
 
         if (b == c) return false;
 
         while (((x >> 2) != 0) || ((y >> 2) != 0)) {
-            x >>= 2;
-            if ((x & 1) != b) return false;
+            if (((x >>= 2) & 1) != b) return false;
 
-            y >>= 2;
-            if ((y & 1) != c) return false;
+            if (((y >>= 2) & 1) != c) return false;
         }
 
         return true;
