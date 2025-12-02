0cd3b330ea8b4ad643384470eac6be956015f4ad
Precious Agbagharauka <clickpresh1@gmail.com>
2025-12-02 11:49:29 +0100

    Create StringBuilder_Simulation.java

---
+++ Base7/StringBuilder_Simulation.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String convertToBase7(int num) {
+        if (num == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+
+        int k = Math.abs(num);
+
+        while (k!= 0) {
+            sb.append(k % 7);
+            k /= 7;
+        } 
+
+        if (num < 0) sb.append('-');
+
+        return sb.reverse().toString(); 
+    }
+}
