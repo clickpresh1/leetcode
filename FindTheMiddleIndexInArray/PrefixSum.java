0b7237f65806db207ee03e3ccb08126eb1f4fea0
Precious Agbagharauka <clickpresh1@gmail.com>
2025-12-09 20:27:24 +0100

    Create PrefixSum.java

---
+++ FindTheMiddleIndexInArray/PrefixSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int m = nums.length, sum = 0, leftSum = 0;
+
+        for (int i = 0; i < m; i++) sum += nums[i];
+
+        for (int i = 0; i < m; i++) {
+            int rightSum = sum - leftSum - nums[i];
+
+            if (rightSum == leftSum) return i;
+
+            leftSum += nums[i];
+        } 
+
+        return -1;
+    }
+}
