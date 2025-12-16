d64f454def947f1a2f575131e08bd9d97682a636
Precious Agbagharauka <clickpresh1@gmail.com>
2025-12-16 20:03:16 +0100

    Update Stack.java

--- BaseballGame/Stack.java
+++ BaseballGame/Stack.java
@@ -3,12 +3,12 @@ class Solution {
         Stack<Integer> q = new Stack<>();
 
         for (int i = 0; i < operations.length; i++) {
-            if (operations[i].equals("+")) {
+            if (operations[i].charAt(0) == '+') {
                 int temp1 = q.pop(), temp2 = q.peek() + temp1;
                 q.push(temp1);
                 q.push(temp2);
-            } else if (operations[i].equals("D")) q.push(q.peek() * 2);
-            else if (operations[i].equals("C")) q.pop();
+            } else if (operations[i].charAt(0) == 'D') q.push(q.peek() * 2);
+            else if (operations[i].charAt(0) == 'C') q.pop();
             else q.push(Integer.valueOf(operations[i]));
         } 
 
