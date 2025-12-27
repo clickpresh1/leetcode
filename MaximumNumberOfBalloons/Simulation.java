072b5d3bf98d2e73818e76fe8f32c04facdac2ff
Precious Agbagharauka <clickpresh1@gmail.com>
2025-12-27 07:35:51 +0100

    Create Simulation.java

---
+++ MaximumNumberOfBalloons/Simulation.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        String bal = "balloon";
+        int m = text.length(), n = bal.length(), min = 10000;
+        int[] freqBal = new int[26], freqText = new int[26];
+
+        for (char c : bal.toCharArray()) freqBal[c - 'a']++;
+
+        for (char c : text.toCharArray()) freqText[c - 'a']++;
+
+        for (char c : bal.toCharArray()) {
+            if (freqText[c - 'a'] >= freqBal[c - 'a']) {
+                int z = freqText[c - 'a'] / freqBal[c - 'a'];
+                if (z < min) min = z;
+            } else return 0;
+        }
+
+        return min;
+    }
+}
