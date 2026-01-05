9ff7719d0a4b5fd449bbb98c062679435065de91
Precious Agbagharauka <clickpresh1@gmail.com>
2026-01-05 15:19:06 +0100

    Create Simulation.java

---
+++ CountOddNumbersInAnIntervalRange/Simulation.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        if (high % 2 == 0) high--;
+        if (low % 2 == 0) low++;
+
+        return ((high - low) / 2) + 1;
+    }
+}
