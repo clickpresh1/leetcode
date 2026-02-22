5080570c42e9024451c2cf7716156e4e784115aa
Precious Agbagharauka <clickpresh1@gmail.com>
2026-02-22 12:00:47 +0100

    Create Simulation.java

---
+++ Search_A_2D_Matrix2/Simulation.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int x = 0, y = n - 1;
+
+        while ((x < m) && (y >= 0)) {
+            int curr = matrix[x][y];
+
+            if (curr == target) return true;
+            else if (curr > target) y--;
+            else x++;
+        } 
+
+        return false;
+    }
+}
