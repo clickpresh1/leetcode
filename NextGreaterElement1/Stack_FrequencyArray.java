e3d2281e0b9ae38c9e2f450ec9355589af6f4158
Precious Agbagharauka <clickpresh1@gmail.com>
2026-03-09 19:51:01 +0100

    Create Stack_FrequencyArray.java

---
+++ NextGreaterElement1/Stack_FrequencyArray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] pam = new int[10001];
+
+        for (int i = 0; i < n; i++) {
+            int item = nums2[i];
+
+            while ((!stack.isEmpty()) && (item > stack.peek())) {
+                int f = stack.pop();
+
+                pam[f] = item + 1;
+            }
+
+            stack.push(item);
+        } 
+
+        for (int i = 0; i < m; i++) {
+            int f = pam[nums1[i]] - 1;
+            
+            nums1[i] = (f > 0) ? f : - 1; 
+        } 
+
+        return nums1;
+    }
+}
