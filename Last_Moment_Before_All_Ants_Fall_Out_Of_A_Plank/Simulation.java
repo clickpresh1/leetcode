5ffc9ce9e144d4717ec138a937cfbc9ffa6f1ef0
Precious Agbagharauka <clickpresh1@gmail.com>
2026-03-20 10:17:52 +0100

    Update Simulation.java

--- Last_Moment_Before_All_Ants_Fall_Out_Of_A_Plank/Simulation.java
+++ Last_Moment_Before_All_Ants_Fall_Out_Of_A_Plank/Simulation.java
@@ -2,17 +2,9 @@ class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
         int m = n, mx = 0;
 
-        for (int item : left) {
-            int time = item - 0;
+        for (int item : left) mx = Math.max(item, mx);
 
-            if (time > mx) mx = time;
-        } 
-
-        for (int item : right) {
-            int time = m - item;
-
-            if (time > mx) mx = time;
-        } 
+        for (int item : right) mx = Math.max(m - item, mx);
 
         return mx;
     }
